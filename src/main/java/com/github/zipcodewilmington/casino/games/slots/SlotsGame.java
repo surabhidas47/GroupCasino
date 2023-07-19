115be022748ee1a238198ec8cead8036e8fb3b0f
Angela Kim <aangelakkim@gmail.com>
2023-07-19 19:03:32 -0400

    edit

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -4,4 +4,16 @@ package com.github.zipcodewilmington.casino.games.slots;
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
+
+    //constructor
+
+    //run method
+
+    /*
+    type 'Y' to pull leveler
+
+    @getResultsOutput
+     */
+
+    //get
 }
