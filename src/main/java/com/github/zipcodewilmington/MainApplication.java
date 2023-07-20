be6777775a54e69c46060a15f09f078fc1a7c1e5
Angela Kim <aangelakkim@gmail.com>
2023-07-20 14:58:45 -0400

    testing merging games to console

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -4,11 +4,6 @@ import java.io.IOException;
 
 public class MainApplication {
     public static void main(String[] args) throws IOException {
-
-        Casino trilliumCasinp = new Casino();
-
-        trilliumCasinp.run();
-
-        //new Casino().run();
+        new Casino().run();
     }
 }
--- src/main/java/com/github/zipcodewilmington/casino/games/rockpaperscissors/RpsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/rockpaperscissors/RpsGame.java
@@ -1,4 +1,36 @@
 package com.github.zipcodewilmington.casino.games.rockpaperscissors;
 
-public class RpsGame {
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.Player;
+
+public class RpsGame implements GameInterface {
+    @Override
+    public void add(Player player) {
+
+    }
+
+    @Override
+    public void remove(Player player) {
+
+    }
+
+    @Override
+    public String displayInstructions() {
+        return null;
+    }
+
+    @Override
+    public Boolean checkWinner() {
+        return null;
+    }
+
+    @Override
+    public void quit() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
