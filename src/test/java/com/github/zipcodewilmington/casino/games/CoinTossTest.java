1113d01024d67361e332f4235e878e07df68e84e
surabhi <surabhi.das@outlook.com>
2023-07-21 15:19:16 -0400

    tests for coin toss

--- src/test/java/com/github/zipcodewilmington/casino/games/CoinTossTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/CoinTossTest.java
@@ -1,6 +1,39 @@
 package com.github.zipcodewilmington.casino.games;
 
+import com.github.zipcodewilmington.casino.games.CoinToss.CoinTossGame;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+
 public class CoinTossTest {
+
+@Test
+    public void testTheToss1(){
+
+    CoinTossGame ct = new CoinTossGame();
+    int result = ct.theToss();
+
+    Assertions.assertTrue(result ==0||result ==1);
+
+
+}
+//@Test
+//public void testTheTossSecific(){
+//    CoinTossGame ct = new CoinTossGame();
+//    int result = ct.theToss();
+//
+//     Assertions.assertTrue(result ==0||result ==1,"The res")
+//
+//
+//
+//
+//
+//}
+//
+
+
+
+
 
 
 }
