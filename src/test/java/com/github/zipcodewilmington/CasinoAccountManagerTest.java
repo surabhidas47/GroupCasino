f8de617093a0d492a3d65a40e27955d5c6046f3a
Angela Kim <aangelakkim@gmail.com>
2023-07-24 17:50:07 -0400

    more tests for account manager

--- src/test/java/com/github/zipcodewilmington/CasinoAccountManagerTest.java
+++ src/test/java/com/github/zipcodewilmington/CasinoAccountManagerTest.java
@@ -130,4 +130,106 @@ public class CasinoAccountManagerTest {
         Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testAddBalance() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer expected = 2000;
+        account.addBalance(500);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddBalance2() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 500);
+        Integer expected = 2000;
+        account.addBalance(1500);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testAddBalance3() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 800);
+        Integer expected = 5200;
+        account.addBalance(4400);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWithdrawBalance() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer expected = 1000;
+        account.withdrawBalance(500);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWithdrawBalance2() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer expected = 500;
+        account.withdrawBalance(1000);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWithdrawBalance3() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 2800);
+        Integer expected = 2200;
+        account.withdrawBalance(600);
+        Integer actual = account.getBalance();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCheckBalance() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer bet = 1000;
+
+        Assert.assertTrue(account.checkBalance(bet));
+    }
+
+    @Test
+    public void testCheckBalance2() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer bet = 1500;
+
+        Assert.assertTrue(account.checkBalance(bet));
+    }
+
+    @Test
+    public void testCheckBalance3() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer bet = 2000;
+
+        Assert.assertFalse(account.checkBalance(bet));
+    }
+
+    @Test
+    public void testCheckBalance4() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount account = cam.createAccount("Bob", "pw", 1500);
+        Integer bet = 1501;
+
+        Assert.assertFalse(account.checkBalance(bet));
+    }
+
 }
