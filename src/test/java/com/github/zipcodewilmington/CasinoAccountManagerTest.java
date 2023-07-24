792e8ebbf157a068c68291d5bba91022d5c596b0
Angela Kim <aangelakkim@gmail.com>
2023-07-24 16:42:30 -0400

    more tests to account manager

--- src/test/java/com/github/zipcodewilmington/CasinoAccountManagerTest.java
+++ src/test/java/com/github/zipcodewilmington/CasinoAccountManagerTest.java
@@ -11,6 +11,7 @@ public class CasinoAccountManagerTest {
 
     CasinoAccount account = new CasinoAccount("Angela", "password", 1000);
     CasinoAccount account1 = new CasinoAccount("Bob", "pw", 2000);
+    CasinoAccount account2 = new CasinoAccount("Linda", "pw", 500);
 
     @Test
     public void testGetAccount() throws IOException {
@@ -31,6 +32,15 @@ public class CasinoAccountManagerTest {
     }
 
     @Test
+    public void testGetAccount3() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        cam.registerAccount(account2);
+        CasinoAccount actual = cam.getAccount("Linda", "pw");
+
+        Assert.assertEquals(actual, account2);
+    }
+
+    @Test
     public void testCreateAccount() throws IOException {
         CasinoAccountManager cam = new CasinoAccountManager();
         CasinoAccount expected = cam.createAccount("Bob", "pw", 2000);
@@ -44,6 +54,15 @@ public class CasinoAccountManagerTest {
         CasinoAccountManager cam = new CasinoAccountManager();
         CasinoAccount expected = cam.createAccount("Angela", "password", 1000);
         CasinoAccount actual = cam.getAccount("Angela", "password");
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCreateAccount3() throws IOException {
+        CasinoAccountManager cam = new CasinoAccountManager();
+        CasinoAccount expected = cam.createAccount("Linda", "pw", 500);
+        CasinoAccount actual = cam.getAccount("Linda", "pw");
 
         Assert.assertEquals(expected, actual);
     }
